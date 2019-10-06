ed2856a7d610eb30886785133b3badae95ea506c
Vagner Clementino <vagner.clementino@gmail.com>
2019-10-06 10:52:13 -0300

    Change Equipment entity

--- Exercise/src/main/java/energy/viridis/exercise/model/Equipment.java
+++ Exercise/src/main/java/energy/viridis/exercise/model/Equipment.java
@@ -7,9 +7,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Equipment {
 
